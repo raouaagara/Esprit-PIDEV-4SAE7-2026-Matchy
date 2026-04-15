18562456d19d63739b283d1e93a6d66531534613
raouaagara <Raoua.gara@esprit.tn>
2026-04-15 04:04:03 +0100

    update environment configs

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.matchy.tn/api',
   wsUrl: 'https://api.matchy.tn/ws',
+  recaptchaSiteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
   appName: 'Matchy',
   version: '1.0.0'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   // ✅ Passe par la Gateway (8080) et non le backend directement (8081)
   apiUrl: 'http://localhost:8080/api',
   wsUrl: 'http://localhost:8080/ws',
+  recaptchaSiteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
   appName: 'Matchy',
   version: '1.0.0'
 };
