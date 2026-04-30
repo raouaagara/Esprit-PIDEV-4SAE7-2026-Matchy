a9f3fd5aae20dc989383d4cdd3f704c3c626d461
salmahaouarii <salmahaouari6@gmail.com>
2026-04-30 01:22:54 +0100

    Add discovery server configuration

--- backend/src/main/java/com/marketplace/MarketplaceApplication.java
+++ backend/src/main/java/com/marketplace/MarketplaceApplication.java
@@ -3,13 +3,15 @@ package com.marketplace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync  // ← ajoute cette ligne
+@EnableAsync
 @EnableCaching
+@EnableDiscoveryClient
 public class MarketplaceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarketplaceApplication.class, args);
